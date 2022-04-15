3f9a710d4ef5fef492a42fc287824f1402d385f9
mr.adumatta@gmail.com <mr.adumatta@gmail.com>
2022-04-15 16:40:10 +0000

    ready to deploy

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # from _typeshed import NoneType
+from email.policy import default
 from json.tool import main
 from unicodedata import category
 from flask import Flask,redirect,url_for,render_template,request
@@ -43,7 +44,7 @@ class Posts(db.Model):
     content = db.Column(db.String)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     author = db.Column(db.String)
-    image_file = db.Column(db.String(200), default='default.png')
+    image_file = db.Column(db.String(2000), default='default.png')
     
     def __repr__(self):
         return f"Posts('{self.id}', '{self.title}')"
@@ -58,11 +59,12 @@ class Candidates(db.Model):
     description = db.Column(db.String)
     number = db.Column(db.String(), default=0)
     age = db.Column(db.String)
-    institution = db.Column(db.String())
+    institution = db.Column(db.String())    
     votes = db.Column(db.Integer, default = 0)
-    image_file = db.Column(db.String(), default='default.png')
-    picture = db.Column(db.String(), default='default.png')
+    image_file = db.Column(db.String(2000), default='default.png')
+    picture = db.Column(db.String(2000), default='default.png')
     updatedVotes = db.Column(db.Integer, default = 0)
+    testField = db.Column(db.String(2000), default="Apart from counting words and characters, our online editor can help you to improve word choice and writing style, and, optionally, help you to detect grammar mistakes and plagiarism. To check word count, simply place your cursor into the text box above and start typing. You'll see the number of characters and words increase or decrease as you type, delete, and edit them. You can also copy and paste text from another program over into the online editor above. The Auto-Save feature will make sure you won't lose any changes while editing, even if you leave the site and come back later. Tip: Bookmark this page now.Knowing the word count of a text can be important. For example, if an author has to write a minimum or maximum amount of words for an article, essay, report, story, book, paper, you name it. WordCounter will help to make sure its word count reaches a specific requirement or stays within a certain limit.In addition, WordCounter shows you the top 10 keywords and keyword density of the article you're writing. This allows you to know which keywords you use how often and at what percentages. This can prevent you from over-using certain words or word combinations and check for best distribution of keywords in your writing.In the Details overview you can see the average speaking and reading time for your text, while Reading Level is an indicator of the education level a person would need in order to understand the words you’re using.Disclaimer: We strive to make our tools as accurate as possible but we cannot guarantee it will always be so.")
     
     # approved = db.Column(db.Boolean, default=False, nullable=False)
 
@@ -184,7 +186,7 @@ def addcontestant():
     subcategories = SubCategory.query.all()
     if form.validate_on_submit():
         print("validated successful")
-        newForm = Candidates(name=form.name.data, age="21", category=form.category.data, description=form.description.data, institution=form.institution.data, image_file=form.picture.data, votes=form.votes.data, number=form.number.data)
+        newForm = Candidates(name=form.name.data, age="21", category=form.category.data, description=form.description.data, institution=form.institution.data, image_file="form.picture.data", votes=form.votes.data, number=form.number.data, testField = form.picture.data)
         db.session.add(newForm)
         db.session.commit()
         flash(f' ' + form.name.data + ' has been nominated for review', 'success')
